f5141383ffe220d04de5b55c04dce2c69d2bac7a
Game Center <bonitaengel@Bonita-Engels-MacBook-Pro.local>
2012-12-13 02:05:19 -0500

    mother fucking yolo polo yolk polk yoko polk ohno polo.

--- Assets/Scripts/PlayerOneFollow.js
+++ Assets/Scripts/PlayerOneFollow.js
@@ -8,5 +8,5 @@ function Start () {
 
 function FixedUpdate () {
 	// in this case I only needed to care about the height
-	transform.position.x= Mathf.Lerp(transform.position.x, Player.transform.position.x + 18, Time.deltaTime); 
+	transform.position.x= Mathf.Lerp(transform.position.x, Player.transform.position.x + 38, Time.deltaTime); 
 }
