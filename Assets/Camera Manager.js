88b8f21cb6b376d80036f0ebd6941eb362061e7d
Game Center <bonitaengel@Bonita-Engels-MacBook-Pro.local>
2012-12-13 12:42:54 -0500

    Adding Camera Manager Script

---
+++ Assets/Camera Manager.js
@@ -0,0 +1,41 @@
+#pragma strict
+
+var cam1: Camera;
+var cam2: Camera;
+var cam3: Camera;
+var player: GameObject;
+var target: GameObject;
+
+function Start() {
+    cam1.enabled = true;
+    cam2.enabled = true;
+    cam3.enabled = false;
+}
+
+function Update() {
+
+	if(player.transform.position.x - target.transform.position.x < (Screen.width / 4)){
+	
+	cam1.enabled = false;
+	cam2.enabled = false;
+	cam3.enabled = true;
+	
+	}
+/*
+    if (Input.GetKeyDown(KeyCode.V) && (cam1.enabled == true || cam3.enabled == true)) {
+    cam1.enabled = false;
+    cam2.enabled = true;
+    cam3.enabled = false;
+    }
+    else if (Input.GetKeyDown(KeyCode.B) && (cam2.enabled == true || cam1.enabled == true)) {
+    cam1.enabled = false;
+    cam2.enabled = false;
+    cam3.enabled = true;
+    }
+    else if (Input.GetKeyDown(KeyCode.C) && (cam2.enabled == true || cam3.enabled == true)) {
+    cam1.enabled = true;
+    cam2.enabled = false;
+    cam3.enabled = false;
+    }
+    */
+}
